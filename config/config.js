c382ee914c64102d1bfcd305d28a693606bc0fff
Emmett O'Sullivan <emmett.j.osullivan@gmail.com>
2017-03-23 19:21:54 +0000

    Updated settiings to switch for different environments.

--- config/config.js
+++ config/config.js
@@ -1,6 +1,16 @@
 "use strict";
 
-module.exports = {
-	secret: "T2LgbxgyG#RwvMeo",
-	database: "mongodb://admin:q6iMFDa.@ds137530.mlab.com:37530/carraig_og_register"
-};
+module.exports = function () {
+    switch (process.env.NODE_ENV) {
+        case 'production': 
+            return {
+	            secret: "T2LgbxgyG#RwvMeo",
+	            database: "mongodb://admin:q6iMFDa.@ds137530.mlab.com:37530/carraig_og_register"
+            };
+        default: 
+            return {
+	            secret: "Carra1g0gisTheb3ST",
+	            database: "mongodb://localhost:27017/CarraigOgRegister"
+            }
+    }
+}();
