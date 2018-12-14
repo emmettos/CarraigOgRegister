de17a1f1a00bfc4f43070b3153239f8d102148f2
Emmett O'Sullivan <emmett.j.osullivan@gmail.com>
2018-12-14 12:54:51 +0000

    Added GroupsService unit tests

--- src/app/_services/_unit-tests/groups.service.spec.ts
+++ src/app/_services/_unit-tests/groups.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+import { IGroup } from '../../_models';
 import { GroupsService } from '../index';
 
 
@@ -35,6 +36,73 @@ describe('GroupsService', () => {
     const mockRequest = httpMock.expectOne('/api/groupSummaries');
 
     expect(mockRequest.request.method).toEqual("GET");
+
+    mockRequest.flush(null);
+  });
+
+  it('should call url for read groups', () => {
+    service.readGroups()
+      .subscribe();
+
+    const mockRequest = httpMock.expectOne('/api/groups');
+
+    expect(mockRequest.request.method).toEqual("GET");
+
+    mockRequest.flush(null);
+  });
+
+  it('should call url for update group', () => {
+    service.updateGroup(null)
+      .subscribe();
+
+    const mockRequest = httpMock.expectOne('/api/updateGroup');
+
+    expect(mockRequest.request.method).toEqual("POST");
+
+    mockRequest.flush(null);
+  });
+
+  it('should pass request body for update group', () => {
+    let group: IGroup = {
+      '_id': 'dfe674827f95ff37765ba0fc',
+      'year': 2018,
+      'name': 'Under 10',
+      'yearOfBirth': 2008,
+      'footballCoach': 'angel_klein@carraigog.com',
+      'hurlingCoach': 'heddwyn_cunningham@carraigog.com',
+      'lastUpdatedDate': '2018-02-27T15:57:21.582Z',
+      'createdBy': 'script',
+      'createdDate': '2017-03-15T13:43:51.268Z',
+      'updatedDate': '2018-02-27T15:57:21.582Z',
+      'updatedBy': 'admin@carraigog.com',
+      '__v': 0,
+      'footballCoachFullName': 'Angel Klein',
+      'hurlingCoachFullName': 'Heddwyn Cunningham'
+    };
+
+    service.updateGroup(group)
+      .subscribe();
+
+    const mockRequest = httpMock.expectOne('/api/updateGroup');
+
+    expect(mockRequest.request.body).toEqual({
+      groupDetails: {
+        '_id': 'dfe674827f95ff37765ba0fc',
+        'year': 2018,
+        'name': 'Under 10',
+        'yearOfBirth': 2008,
+        'footballCoach': 'angel_klein@carraigog.com',
+        'hurlingCoach': 'heddwyn_cunningham@carraigog.com',
+        'lastUpdatedDate': '2018-02-27T15:57:21.582Z',
+        'createdBy': 'script',
+        'createdDate': '2017-03-15T13:43:51.268Z',
+        'updatedDate': '2018-02-27T15:57:21.582Z',
+        'updatedBy': 'admin@carraigog.com',
+        '__v': 0,
+        'footballCoachFullName': 'Angel Klein',
+        'hurlingCoachFullName': 'Heddwyn Cunningham'
+        }
+    });
 
     mockRequest.flush(null);
   });
